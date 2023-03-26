866a8d5f8b9da623856fed37d083cbe7fdac50eb
Shamakhan <valentin-u-a@yandex.ru>
2023-03-26 23:46:18 +0300

    fix: Исправлены замечания CheckStyle

--- stats/stats-server/src/main/java/ru/practicum/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/StatsController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
