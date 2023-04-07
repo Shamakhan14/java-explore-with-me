f4078b95ee86a55aeea0531b8aeeb0b52b537452
Shamakhan <valentin-u-a@yandex.ru>
2023-04-08 01:02:11 +0300

    feat: Исправлены замечания CheckStyle

--- main/src/main/java/ru/practicum/event/dto/UpdateEventUserRequest.java
+++ main/src/main/java/ru/practicum/event/dto/UpdateEventUserRequest.java
@@ -1,11 +1,6 @@
 package ru.practicum.event.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
-import ru.practicum.event.model.Location;
-
-import javax.validation.constraints.Size;
-import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
